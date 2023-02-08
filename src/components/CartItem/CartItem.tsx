df1427cad8fb58949e1f22b9421d606367f55d21
Ayo Sam <ayotolusam@gmail.com>
2023-02-08 12:46:16 +0100

    Shopping cart menu design

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -1,4 +1,7 @@
 import { useShoppingCart } from "@/contexts/ShoppingCartContext"
+import storeItems from "../../data/items.json"
+import Image from "next/image"
+import {IoMdClose} from 'react-icons/io'
 
 type CartItemProps = {
   id: number,
@@ -6,9 +9,34 @@ type CartItemProps = {
 }
 
 const CartItem = ({id, quantity} : CartItemProps) => {
+
+  const { removeFromCart } = useShoppingCart()
+  const item = storeItems.find(item => item.id === id)
+
+  if (item == null) return null
+
   return (
-    <div>
-      
+    <div className="cartItem">
+      <Image className="itemImg" width="1200" height="200" src={item.imgUrl} alt="cart item" />
+      <div className="itemInfo">
+        <div className="infoLeft">
+          <div className="itemDetails">
+            <h4>
+              {item.name}
+            </h4>
+            <p>
+              {"x" + quantity}
+            </p>
+          </div>
+          <p className="itemPrice">
+            ${item.price}
+          </p>
+        </div>
+        <div className="infoRight">
+          <h4>{item.price * quantity}</h4>
+          <button onClick={() => removeFromCart(id)}><IoMdClose/></button>
+        </div>
+      </div>
     </div>
   )
 }
